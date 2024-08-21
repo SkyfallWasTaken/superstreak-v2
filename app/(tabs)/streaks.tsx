2f6065c3f2d884cfb53f364ddca20e6c3d685b67
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-21 23:41:09 +0100

    Migrate to FlatList

--- app/(tabs)/streaks.tsx
+++ app/(tabs)/streaks.tsx
@@ -10,6 +10,10 @@ import {
   YStack,
 } from "tamagui";
 import { StreakCard } from "app/components/index/StreakCard";
+import { FlashList } from "@shopify/flash-list";
+import { FlatList, View } from "react-native";
+
+const Separator = () => <View style={{ height: 16 }} />; // Adjust height as needed
 
 export default function StreaksScreen() {
   const media = useMedia();
@@ -24,13 +28,21 @@ export default function StreaksScreen() {
       completed: false,
       id: 0,
     },
+    {
+      name: "Do the dishes",
+      iconEmoji: "🍽️",
+      description: "Wash the dishes in the kitchen",
+      streak: 3,
+      completed: false,
+      id: 1,
+    },
   ];
   const streakCards = streaks.map((streak) => (
     <StreakCard {...streak} key={streak.id} />
   ));
-
+  //
   return (
-    <YStack f={1} ai="center" gap="$8" px="$10" pt="$5">
+    <YStack jc="center" gap="$8" pt="$6" px="$5">
       <H2>Tamagui + Expo</H2>
 
       <XStack gap="$3" jc="center">
@@ -47,8 +59,12 @@ export default function StreaksScreen() {
       </XStack>
 
       {isMobile ? (
-        <YStack gap="$4" padding="$4">
-          {streakCards}
+        <YStack gap="$4">
+          <FlatList
+            data={streaks}
+            renderItem={({ item }) => <StreakCard {...item} key={item.id} />}
+            ItemSeparatorComponent={Separator}
+          />
         </YStack>
       ) : (
         <YStack gap="$4" padding="$4">
@@ -57,48 +73,6 @@ export default function StreaksScreen() {
           </XStack>
         </YStack>
       )}
-
-      <XStack
-        ai="center"
-        jc="center"
-        fw="wrap"
-        gap="$1.5"
-        pos="absolute"
-        b="$8"
-      >
-        <Paragraph fos="$5">Add</Paragraph>
-
-        <Paragraph fos="$5" px="$2" py="$1" col="$blue10" bg="$blue5" br="$3">
-          tamagui.config.ts
-        </Paragraph>
-
-        <Paragraph fos="$5">to root and follow the</Paragraph>
-
-        <XStack
-          ai="center"
-          gap="$1.5"
-          px="$2"
-          py="$1"
-          br="$3"
-          bg="$purple5"
-          hoverStyle={{ bg: "$purple6" }}
-          pressStyle={{ bg: "$purple4" }}
-        >
-          <Anchor
-            href="https://tamagui.dev/docs/core/configuration"
-            textDecorationLine="none"
-            col="$purple10"
-            fos="$5"
-          >
-            Configuration guide
-          </Anchor>
-          <ExternalLink size="$1" col="$purple10" />
-        </XStack>
-
-        <Paragraph fos="$5" ta="center">
-          to configure your themes and tokens.
-        </Paragraph>
-      </XStack>
     </YStack>
   );
 }
