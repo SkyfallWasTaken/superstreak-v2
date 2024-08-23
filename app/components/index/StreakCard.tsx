f3d5b5451524f5d0e5f735d62658094c5d2f4e23
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-23 20:01:14 +0100

    Add deletion + incrementation of streaks

--- app/components/index/StreakCard.tsx
+++ app/components/index/StreakCard.tsx
@@ -1,9 +1,10 @@
-import { Check } from "@tamagui/lucide-icons";
-import type { Streak } from "app/streaks";
+import { Check, Trash2 } from "@tamagui/lucide-icons";
+import { useStreaksStore, type Streak } from "app/streaks";
 import { Button, Card, H2, Separator, Text, useTheme, XStack } from "tamagui";
 
 export function StreakCard(props: Streak) {
   const theme = useTheme();
+  const store = useStreaksStore();
 
   return (
     <Card
@@ -20,6 +21,13 @@ export function StreakCard(props: Streak) {
         <XStack gap="$3">
           <H2 fos="$8">{props.iconEmoji}</H2>
           <H2 fos="$8">{props.name}</H2>
+          <Button
+            circular
+            icon={<Trash2 size="$1.5" color="$gray8" />}
+            bg="$colorTransparent"
+            ml="auto"
+            onPress={() => store.removeStreak(props.id)}
+          />
         </XStack>
         <Text
           fos="$5"
@@ -27,14 +35,21 @@ export function StreakCard(props: Streak) {
           mb={props.description && "$2"}
           fow="700"
         >
-          {props.streak} days completed
+          {props.streak || "No"} days completed
         </Text>
         {props.description && <Text>{props.description}</Text>}
         <Separator mt={15} />
       </Card.Header>
       <Card.Footer px="$4" pb="$4">
         <XStack gap="$3" jc="center">
-          <Button circular bg={theme.green11} icon={<Check size="$2" />} />
+          <Button
+            circular
+            bg={theme.green11}
+            icon={<Check size="$2" />}
+            onPress={() =>
+              store.updateStreak(props.id, { streak: props.streak + 1 })
+            }
+          />
         </XStack>
       </Card.Footer>
     </Card>
